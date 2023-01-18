5bef5b51ccce3d0df8470f4fbf15b5f8981793f9
lucasoliveiradossantos <122839107+lucasoliveiradossantos@users.noreply.github.com>
2023-01-18 18:57:03 -0300

    Initial commit

---
+++ urna/urna/exemplo.js
@@ -0,0 +1,19 @@
+function comecarEtapa() {
+    let etapa = etapas[etapaAtual];
+
+    let numeroHtml = '';
+
+    for(let i=0;i<etapa.numeros;i++) {
+        numeroHtml += '<div class="numero"></div>';
+    }
+
+
+    seuVotoPara.style.display = 'none';
+    cargo.innerHTML = etapa.titulo;
+    descricao.innerHTML = '';
+    aviso.style.display = 'none';
+    lateral.innerHTML = '';
+    numeros.innerHTML = numeroHtml;
+}
+
+comecarEtapa();
